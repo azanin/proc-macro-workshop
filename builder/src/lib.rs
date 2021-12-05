17d836ea7d0759626fc75315044d1249aa24965f
azanin <ale.zanin90@gmail.com>
2021-12-05 13:20:42 +0100

    optional fields

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, Ident};
+use syn::{DeriveInput, Ident, Type};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -9,6 +9,43 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).expect("Can't parse the input stream");
 
     impl_derive_builder(&ast)
+}
+
+fn get_generic_from_opt(ty: &Type) -> Option<&Type> {
+    match ty {
+        syn::Type::Path(p) => {
+            if p.qself.is_some() {
+                return None;
+            }
+
+            if p.path.leading_colon.is_some() {
+                return None;
+            }
+
+            let segment = p.path.segments.first().unwrap();
+
+            if segment.ident.to_string() != "Option" {
+                return None;
+            }
+
+            match &segment.arguments {
+                syn::PathArguments::AngleBracketed(arg) => {
+                    if arg.args.len() != 1 {
+                        return None;
+                    } else {
+                        return match arg.args.first().unwrap() {
+                            syn::GenericArgument::Type(gty) => Some(gty),
+                            _ => None,
+                        };
+                    }
+                }
+                _ => {
+                    return None;
+                }
+            }
+        }
+        _ => return None,
+    }
 }
 
 fn impl_derive_builder(derive_input: &DeriveInput) -> TokenStream {
@@ -24,9 +61,15 @@ fn impl_derive_builder(derive_input: &DeriveInput) -> TokenStream {
         let name = field.ident.as_ref().unwrap();
         let ty = &field.ty;
 
-        quote!(
-            #name: std::option::Option<#ty>,
-        )
+        if get_generic_from_opt(ty).is_some() {
+            quote!(
+                #name: #ty
+            )
+        } else {
+            quote!(
+                #name: std::option::Option<#ty>,
+            )
+        }
     });
 
     let init_fields = fields.iter().map(|f| {
@@ -41,20 +84,35 @@ fn impl_derive_builder(derive_input: &DeriveInput) -> TokenStream {
         let name = f.ident.as_ref().unwrap();
         let patameter_type = &f.ty;
 
-        quote!(
-            pub fn #name(&mut self, #name: #patameter_type) -> &mut Self {
-                self.#name = Some(#name);
-                self
-            }
-        )
+        if let Some(gty) = get_generic_from_opt(patameter_type) {
+            quote!(
+                pub fn #name(&mut self, #name: #gty) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            )
+        } else {
+            quote!(
+                pub fn #name(&mut self, #name: #patameter_type) -> &mut Self {
+                    self.#name = Some(#name);
+                    self
+                }
+            )
+        }
     });
 
     let init_struct_from_builder = fields.iter().map(|f| {
         let name = f.ident.as_ref().unwrap();
-
-        quote!(
-            #name: self.#name.as_ref().ok_or("missing field".to_string())?.clone(),
-        )
+        let ty = &f.ty;
+        if let Some(_) = get_generic_from_opt(ty) {
+            quote!(
+             #name: self.#name.clone()
+            )
+        } else {
+            quote!(
+                #name: self.#name.as_ref().ok_or("missing field".to_string())?.clone(),
+            )
+        }
     });
 
     let gen = quote! {
